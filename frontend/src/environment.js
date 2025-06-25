41754d392bb7f148bb0e058545aec41671e721e8
Priya Chauhan <priyachauh5@gmail.com>
2025-06-25 17:49:09 +0530

    Deploy frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://zoom-clone-backend-wbqe.onrender.com/" :
 
     "http://localhost:8000"
 
